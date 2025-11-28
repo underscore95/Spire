d176824903e75be1fa23967fdff5d0a5954966de
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-11-28 12:17:14 +0000

    Dynamic profiling for Test6

--- Spire/SpireVoxel/Source/VoxelRenderer.h
+++ Spire/SpireVoxel/Source/VoxelRenderer.h
@@ -69,13 +69,14 @@ namespace SpireVoxel {
             glm::u64 FramesToProfile;
         };
 
-        static constexpr const char *WORLD_NAME = "Test2B";
+        static constexpr const char *WORLD_NAME = "Test6";
 
         static constexpr ProfileStrategy PROFILE_STATIC = {ProfileStrategy::STATIC, 10000};
         static constexpr ProfileStrategy PROFILE_STATIC_1000 = {ProfileStrategy::STATIC, 1000};
+        static constexpr ProfileStrategy PROFILE_DYNAMIC_10 = {ProfileStrategy::DYNAMIC, 10};
         static constexpr ProfileStrategy PROFILE_DYNAMIC = {ProfileStrategy::DYNAMIC, 100};
         //  static constexpr std::array<ProfileStrategy, 2> PROFILE_STRATEGIES = {PROFILE_STATIC, PROFILE_DYNAMIC};
-        static constexpr std::array<ProfileStrategy, 1> PROFILE_STRATEGIES = {PROFILE_STATIC_1000};
+        static constexpr std::array<ProfileStrategy, 2> PROFILE_STRATEGIES = {PROFILE_STATIC_1000, PROFILE_DYNAMIC_10};
 
         Spire::Engine &m_engine;
         VkShaderModule m_vertexShader = VK_NULL_HANDLE;
