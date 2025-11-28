37719047f774f3e5dc67b420b8a628f50523f6df
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-11-28 13:17:56 +0000

    Profiled no face culling, pregen pass to build bitmasks

--- Spire/SpireVoxel/Source/VoxelRenderer.h
+++ Spire/SpireVoxel/Source/VoxelRenderer.h
@@ -56,8 +56,8 @@ namespace SpireVoxel {
         void HandleProfiling();
 
     public:
-        static constexpr bool IS_PROFILING = false;
-        static constexpr bool RENDER_WIREFRAMES = false;
+        static constexpr bool IS_PROFILING = true;
+        static constexpr bool RENDER_WIREFRAMES = true;
 
     private:
         struct ProfileStrategy {
