481d55e59c54d4c7a941f90d44586ccdd6e4e2a6
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-12-03 16:56:39 +0000

    Profiling

--- Spire/SpireVoxel/Source/Rendering/VoxelWorldRenderer.h
+++ Spire/SpireVoxel/Source/Rendering/VoxelWorldRenderer.h
@@ -46,7 +46,7 @@ namespace SpireVoxel {
     private:
         static constexpr glm::u32 MAXIMUM_VERTICES_IN_WORLD = 36 * (SPIRE_VOXEL_CHUNK_VOLUME * 48);
         static constexpr glm::u32 MAXIMUM_LOADED_CHUNKS = 384 * 3;
-        static constexpr glm::u32 MAXIMUM_RENDERED_VOXELS = MAXIMUM_VERTICES_IN_WORLD / 16; // maximum amount of voxels where at least 1 face is rendered
+        static constexpr glm::u32 MAXIMUM_RENDERED_VOXELS = MAXIMUM_VERTICES_IN_WORLD / 8; // maximum amount of voxels where at least 1 face is rendered
 
         VoxelWorld &m_world;
 
